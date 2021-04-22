ef2d589ed6ac66ff5579633f8755b424b46d34b5
guilherj <contato.guilhermenascimento@hotmail.com>
2021-04-22 11:56:25 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +14,17 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scrollPane = loader.load();
+			
+			/*
+			 * Macete para ajustar via código o scroll pane na janela.
+			 * Assim pode redimensionar a janela o quanto quiser que o scroll pane
+			 * Vai ocupar as bordas da forma correta.
+			 */
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+						
+			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
